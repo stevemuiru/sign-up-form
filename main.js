d7fc23213e4e3f80f87b5fe73f0b0e3243738002
stephen muiru <stevembugua26@gmail.com>
2024-04-12 10:28:59 +0300

    code additions

---
+++ main.js
@@ -0,0 +1,11 @@
+ssdocument.addEventListener("DOMContentLoaded", function(){
+    let password = document.getElementById("#password").value;
+    let confirmedPassword = document.getElementById("#confirmPassword").value;
+
+    let error = document.getElementById("#error")
+
+    if (password != confirmedPassword) {
+        error.innerHTML = "*Passwords do not match."
+    }
+
+})
